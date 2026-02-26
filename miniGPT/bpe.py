4687b108b36fbc6920728c85b720c28f3cd55b05
cyyang <yueyangc67@gmail.com>
2026-02-26 21:32:19 +0800

    finish Encoder

--- miniGPT/bpe.py
+++ miniGPT/bpe.py
@@ -1,3 +1,5 @@
+from secrets import token_bytes
+
 import torch
 import os
 import json
@@ -22,4 +24,115 @@ def bytes_to_unicode():
 
     return d
 
+def get_pairs(word):
+    """
+    返回单词被切分为pairs, eg. (w, o, r, d) -> {(w, o), (o, r), (r, d)}
+    """
+    pairs = set()
+    prev_char = word[0]
+    for char in word[1:]:
+        pairs.add((prev_char, char))
+        prev_char = char
+    return pairs
+
+class Encoder():
+    def __init__(self, encoder, bpe_merges):
+        self.byte_encoder = bytes_to_unicode()
+        self.byte_decoder = {v: k for k, v in self.byte_encoder.items()}
+        self.encoder = encoder   # token字符串-> token id
+        self.decoder = {v: k for k, v in self.encoder.items()} # token id -> token 字符串
+
+        self.bpe_ranks = dict(zip(bpe_merges, range(len(bpe_merges))))
+
+        self.pat = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")
+        self.cache = {}
+
+    def bpe(self, token):
+        if token in self.cache:
+            return self.cache[token]
+
+        word = tuple(token)
+        pairs = get_pairs(word)
+
+        # token 只有单个字符
+        if not pairs:
+            return token
+
+        while True:
+            bigram = min(pairs, key=lambda x: self.bpe_ranks.get(x, float("inf")))
+            if bigram not in self.bpe_ranks:
+                break # 没有bigram可以再被合并了
+
+            first, second = bigram
+            new_word = []
+            i = 0
+            while i < len(word):
+                try:
+                    j = word.index(first, i)
+                    new_word.extend(word[i:j])
+                    i = j
+                except:
+                    new_word.extend(word[i:])
+                    break
+
+                if word[i] == first and i < len(word) - 1 and word[i + 1] == second:
+                    new_word.append(first + second)
+                    i += 2
+                else:
+                    new_word.append(word[i])
+                    i += 1
+
+            new_word = tuple(new_word)
+            word = new_word
+
+            if len(word) == 1:
+                break
+            else:
+                pairs = get_pairs(word)
+
+        word = ' '.join(word)
+        self.cache[token] = word
+        return word
+
+    def encode(self, text):
+        bpe_idx = []
+
+        tokens = re.findall(self.pat, text)
+        for token in tokens:
+            token_bytes = token.encode("utf-8")
+            token_translated = ''.join(self.byte_encoder[b] for b in token_bytes)
+            token_merged = self.bpe(token_translated).split(' ')
+            bpe_idx.extend([self.encoder[bpe_token] for bpe_token in token_merged])
+
+        return bpe_idx
+
+    def encode_and_show_word(self, text):
+        bpe_idx = []
+        parts = []
+        tokens = re.findall(self.pat, text)
+        for token in tokens:
+            token_bytes = token.encode("utf-8")
+            token_translated = ''.join(self.byte_encoder[b] for b in token_bytes)
+            token_merged = self.bpe(token_translated).split(' ')
+            bpe_idx.extend([self.encoder[bpe_token] for bpe_token in token_merged])
+            parts.append({
+                'token': token,
+                'token_bytes': token_bytes,
+                'token_translated': token_translated,
+                'token_merged': token_merged,
+                'token_ix': [self.encoder[bpe_token] for bpe_token in token_merged]
+            })
+        out = {
+            'bpe_idx': bpe_idx,
+            'parts': parts,
+            'tokens': tokens,
+        }
+        return out
+
+    def decode(self, bpe_idx):
+        tokens_merged = [self.decoder[token] for token in bpe_idx]
+        tokens_flat = ''.join(tokens_merged)
+        token_bytes = bytearray([self.byte_decoder[c] for c in tokens_flat])
+        text = token_bytes.decode("utf-8", errors='replace')
+        return text
 
